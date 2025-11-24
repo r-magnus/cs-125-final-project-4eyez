c0e3916c124d2d5814e09cb245489879a833c667
Ryan Magnuson <ryan.magnuson@outlook.com>
2025-11-24 15:21:52 -0800

    un-overwrote schema

--- db/schema.sql
+++ db/schema.sql
@@ -8,27 +8,32 @@ CREATE TABLE Person(
     lastName VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Volunteer(
-    personId INT,
-    FOREIGN KEY (personId) REFERENCES Person(personId)
-);
-
-CREATE TABLE Admin(
-    personId INT,
-    FOREIGN KEY (personId) REFERENCES Person(personId)
-);
-
 CREATE TABLE Guardian(
-    personId INT,
+    personId INT PRIMARY KEY,
     FOREIGN KEY (personId) REFERENCES Person(personId)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE Student(
     personId INT,
     guardianId INT NOT NULL,
     grade CHAR(1) NOT NULL,
-    FOREIGN KEY (personId) REFERENCES Person(personId),
-    FOREIGN KEY (guardianId) REFERENCES Guardian(personId)
+    FOREIGN KEY (personId) REFERENCES Person(personId)
+    ON DELETE CASCADE,
+    FOREIGN KEY(guardianId) REFERENCES Guardian(personId)
+    ON DELETE RESTRICT
+);
+
+CREATE TABLE Volunteer(
+    personId INT,
+    FOREIGN KEY (personId) REFERENCES Person(personId)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Admin(
+    personId INT,
+    FOREIGN KEY (personId) REFERENCES Person(personId)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE Meeting(
@@ -36,38 +41,43 @@ CREATE TABLE Meeting(
     title VARCHAR(100) NOT NULL
 );
 
-
 CREATE TABLE MeetingSignUpItem(
     id INT PRIMARY KEY,
     signeeId INT NOT NULL,
     signedUpById INT NOT NULL,
     meetingId INT NOT NULL,
-    FOREIGN KEY (signedUpById) REFERENCES Person(personId),
-    FOREIGN KEY (signeeId) REFERENCES Person(personId),
+    FOREIGN KEY (signedUpById) REFERENCES Person(personId)
+    ON DELETE RESTRICT,
+    FOREIGN KEY (signeeId) REFERENCES Person(personId)
+    ON DELETE RESTRICT,
     FOREIGN KEY (meetingId) REFERENCES Meeting(meetId)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE NoteItem(
     Id INT PRIMARY KEY,
     writerId INT NOT NULL,
     meetingId INT NOT NULL,
-    textContent VARCHAR(255),
-    FOREIGN KEY (meetingId) REFERENCES Meeting(meetId),
+    textContent TEXT,
+    FOREIGN KEY (meetingId) REFERENCES Meeting(meetId)
+    ON DELETE RESTRICT,
     FOREIGN KEY(writerId) REFERENCES Person(personId)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE attendanceItem(
     id INT PRIMARY KEY,
     signupId INT NOT NULL,
-    STATUS CHAR(1),
-    FOREIGN KEY (signupId) REFERENCES MeetingSignUpItem(signeeId)
+    STATUS CHAR(1) NOT NULL,
+    FOREIGN KEY (signupId) REFERENCES MeetingSignUpItem(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE smallGroup(
     meetId INT NOT NULL,
     nextMeetingDate DATE,
     FOREIGN KEY (meetId) REFERENCES Meeting(meetId)
-
+    ON DELETE CASCADE
 );
 
 CREATE TABLE Event(
@@ -77,6 +87,8 @@ CREATE TABLE Event(
     location VARCHAR(100) NOT NULL,
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    FOREIGN KEY (meetID) REFERENCES Meeting(meetId),
+    FOREIGN KEY (meetID) REFERENCES Meeting(meetId)
+    ON DELETE CASCADE,
     FOREIGN KEY (createdByID) REFERENCES Person(personId)
+    ON DELETE RESTRICT
 );
